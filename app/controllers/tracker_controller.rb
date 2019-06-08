be917d9de7082832103e498a9a4811c49a183ada
dusterg20 <dusterg20@gmail.com>
2019-06-08 09:12:43 -0500

    fix Objects and SQL causing long load time

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -1,6 +1,13 @@
 class TrackerController < ApplicationController
   def index
-    @tracks = Track.find_by_sql('SELECT m.* FROM tracks m LEFT JOIN tracks b ON m.xytagid = b.xytagid AND m.xytimestamp < b.xytimestamp WHERE b.xytimestamp IS NULL')
+    #chh test
+    @tags_with_tracking_events = {}
+    tags = Tag.all
+    tags.each do |tag|
+	    tracking_events = Track.where(xytagid: tag.key_id).order('xytimestamp DESC').limit(5)
+      @tags_with_tracking_events[tag.key_id] = tracking_events
+    end    
+#    @tracks = Track.find_by_sql('SELECT m.* FROM tracks m LEFT JOIN tracks b ON m.xytagid = b.xytagid AND m.xytimestamp < b.xytimestamp WHERE b.xytimestamp IS NULL')
   end
 
   def show
