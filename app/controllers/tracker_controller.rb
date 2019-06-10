64ddc7c9539a93979b2d87ccc6c3850a339de818
dusterg20 <dusterg20@gmail.com>
2019-06-10 16:18:29 -0500

    Added photo background to chart plot

--- app/controllers/tracker_controller.rb
+++ app/controllers/tracker_controller.rb
@@ -5,7 +5,7 @@ class TrackerController < ApplicationController
     tags = Tag.all
     tags.each do |tag|
 	    tracking_events = Track.where(xytagid: tag.key_id).order('xytimestamp DESC').limit(5)
-      @tags_with_tracking_events[tag.key_id] = tracking_events
+      @tags_with_tracking_events[tag.key_id] = tracking_events.to_a
     end    
 #    @tracks = Track.find_by_sql('SELECT m.* FROM tracks m LEFT JOIN tracks b ON m.xytagid = b.xytagid AND m.xytimestamp < b.xytimestamp WHERE b.xytimestamp IS NULL')
   end
